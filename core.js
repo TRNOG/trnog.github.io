3e65e2eaa85ecc013738eff884af5953e9938958
Alptekin Sünnetci <alptekin@sunnetci.net>
2021-11-09 17:06:48 +0300

    Update core.js

--- core.js
+++ core.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
 
+	$(".asUP").on('input', function(evt) {
+			  var input = $(this);
+			  var start = input[0].selectionStart;
+			  $(this).val(function (_, val) {
+			    return val.toUpperCase();
+			  });
+			  input[0].selectionStart = input[0].selectionEnd = start;
+	});
+	
 	$(document).keyup(function(event) {
 		if (event.keyCode == 13) {
 			$("#Submit").click();
